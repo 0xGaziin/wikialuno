f195941330776e28a86ee650637739d52e52ffa6
0xGaziin <gaziin208@gmail.com>
2026-03-20 22:23:45 -0300

    style: implement layout and styling for edit-profile page

--- edit-profile.php
+++ edit-profile.php
@@ -14,15 +14,18 @@ if (!isset($_SESSION['user_logged'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./styles/style.css">
+    <link rel="stylesheet" href="./styles/pages/edit-profile.css">
     <title>Editar o Sobre Mim</title>
 </head>
 <body>
     <?php include './components/header.php' ?>
     
-    <form action="./src/change-profile.php" method="post">
-        <textarea name="description" placeholder='Digite o seu sobre mim'></textarea>
-        <button type="submit">Enviar</button>
-    </form>
+    <main>
+        <form action="./src/change-profile.php" method="post">
+            <textarea name="description" placeholder='Digite o seu sobre mim'></textarea>
+            <button type="submit">Enviar</button>
+        </form>
+    </main>
 
     <?php include './components/footer.php' ?>
 </body>
